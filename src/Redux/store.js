ca439abd14231fdcfc3b11779fe1a2a4e342ee8c
shubham <shubhamv430@gmail.com>
2021-09-28 16:08:50 +0530

    redux auth-reducer

---
+++ src/Redux/store.js
@@ -0,0 +1,18 @@
+import { combineReducers, createStore,applyMiddleware,compose } from "redux";
+import {authReducer} from "./auth/reducer";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+});
+
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk),
+);
+
+export const store = createStore(
+    rootReducer,
+    enhancer
+);
