abf3afeac5c9cac0afc6f67bbc0eb18e5018c5d3
Gaurav <parmargaurav1234@gmail.com>
2021-09-29 16:49:01 +0530

    Registration Modal created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import HomePage from "./Components/HomePage/HomePage";
 import Navbar from "./Components/HomePage/Navbar/Navbar";
-
+// import RegistrationModal from './Components/HomePage/Registration/RegistrationModal'
 function App() {
   return (
     <div className="App" style={{ background: "#dae0e6",}}>
       <Navbar />
       <HomePage />
+      {/* <RegistrationModal /> */}
     </div>
   );
 }
---
+++ src/Components/HomePage/Registration/Login.jsx
@@ -0,0 +1,59 @@
+import React, {useState} from 'react'
+import styles from './RegistrationModal.module.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+const Login = () => {
+    const [loginText, setLoginText] = useState(false);
+    const [username, setUsername] = useState("");
+    const handleLoginText = () => {
+        if(username.length>0 && username.length<3){
+          setLoginText(true);
+        }
+        else{
+          setLoginText(false);
+        }
+    }
+    return (
+        <>
+        
+              <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { mb: 1.2, width: '100%' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+        <div>
+        <TextField
+          id="filled-search"
+          label="USERNAME"
+          type="search"
+          variant="filled"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          
+        />
+       {loginText?<p style={{color:"red", fontSize:"small", marginTop:"-10px"}}>Username must be between 3 and 20 characters</p>:<></>}
+           <TextField
+          id="filled-search"
+          label="PASSWORD"
+          type="search"
+          variant="filled"
+          onClick={handleLoginText}
+          
+        />
+        </div>
+    </Box>
+      <button className={styles.btn}>Log In</button>
+      
+    <div className={styles.afterBtnText1}>
+        <span>Forgot your username or password ?</span>
+    </div>
+        </>
+    )
+}
+
+export default Login;
