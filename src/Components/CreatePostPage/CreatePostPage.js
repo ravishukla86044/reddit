6ea698cfa63fd90dc73bf2f183398d84f364d368
shubham <shubhamv430@gmail.com>
2021-09-30 19:29:50 +0530

    Create Post Page

---
+++ src/Components/CreatePostPage/CreatePostPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import CreatePost  from './Components/CreatePost'
+import PostAbout from './Components/PostAbout'
+import styled from 'styled-components'
+
+const CreatePostPage = () => {
+    return (
+        <StyledPage>
+        <CreatePost/>
+        <PostAbout/>
+        </StyledPage>
+    )
+}
+const StyledPage = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  padding: 40px 90px;
+  &>div{
+      padding: 1rem;
+      border-radius: .5rem;
+      width: 740px;
+      margin-right: 1rem;
+  }
+  &>div+div{
+      padding: 0;
+      width: 315px;
+  }
+`
+
+export default CreatePostPage
