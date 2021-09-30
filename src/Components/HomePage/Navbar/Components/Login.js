a35da7277b9b9973f627c0a1015b2481522cbaa6
shubham <shubhamv430@gmail.com>
2021-09-30 11:51:59 +0530

    navbar login conditional rendering

--- src/Components/HomePage/Navbar/Components/Login.js
+++ src/Components/HomePage/Navbar/Components/Login.js
@@ -10,7 +10,6 @@ import { useSelector } from 'react-redux'
 
 const Login = () => {
     const isAuth = useSelector(state => state.auth.isAuth);
-    console.log('isAuth:', isAuth)
     return (
         <StyleLogin>
             {isAuth ? <>
