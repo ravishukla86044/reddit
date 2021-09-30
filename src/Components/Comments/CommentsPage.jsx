dc457466b6345d6ca7765d9ec33e3c62f2c4f08c
ravishukla86044 <ravishukls86044@gmail.com>
2021-09-30 15:17:54 +0530

    feed calssic component added

--- src/Components/Comments/CommentsPage.jsx
+++ src/Components/Comments/CommentsPage.jsx
@@ -1,6 +1,11 @@
 import { CommentsItem } from "./CommentItem";
 import styled from "styled-components";
 import { FeedItem } from "../HomePage/Feed/FeedItem";
+import { useSelector } from "react-redux";
+import { ImArrowUp, ImArrowDown } from "react-icons/im";
+import { RiFileListLine } from "react-icons/ri";
+import { IoMdClose } from "react-icons/io";
+import { useHistory } from "react-router-dom";
 
 const arr = [
   {
@@ -30,16 +35,44 @@ const arr = [
 ];
 
 function CommentsPage() {
+  const { isLight } = useSelector((state) => state.color);
+  const history = useHistory();
   return (
-    <StyledDiv>
-      <div className="feedDiv">
-        <FeedItem comments={true} />
-        {arr.map((a) => (
-          <CommentsItem data={a} />
-        ))}
+    <StyledDiv isLight={isLight}>
+      <div className="commentHeader">
+        <div>
+          <div className="likeDiv">
+            <Likes isLight={isLight}>
+              <ImArrowUp />
+              <div>2</div>
+              <ImArrowDown />
+            </Likes>
+            <div className="upperTitle">
+              <RiFileListLine />
+              <div>thfdffdsdfdfd</div>
+            </div>
+          </div>
+          <div
+            onClick={() => {
+              history.goBack();
+            }}
+            className="commentClose"
+          >
+            <IoMdClose />
+            <div>Close</div>
+          </div>
+        </div>
       </div>
       <div>
-        <div className="fake"></div>
+        <div className="feedDiv">
+          <FeedItem comments={true} />
+          {arr.map((a) => (
+            <CommentsItem data={a} />
+          ))}
+        </div>
+        <div className="sideDiv">
+          <div className="fake"></div>
+        </div>
       </div>
     </StyledDiv>
   );
@@ -50,15 +83,25 @@ const StyledDiv = styled.div`
   box-sizing: border-box;
   background: rgb(46, 47, 48);
   display: flex;
-  align-items: flex-start;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
   .feedDiv > div:nth-child(1) {
     margin-bottom: -10px;
   }
-  & > div {
+  & > div:nth-child(2) {
+    max-width: 1068px;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    background-color: ${(props) => (props.isLight ? "#DAE0E6" : "#030303")};
+    box-sizing: border-box;
+    padding: 0px 60px;
+  }
+  .feedDiv {
     margin-top: 30px;
   }
-  & > div:nth-child(2) {
+  .sideDiv {
     margin-top: 30px;
     margin-left: 30px;
   }
@@ -66,6 +109,110 @@ const StyledDiv = styled.div`
     width: 300px;
     height: 200px;
     border: 1px solid black;
+  }
+  .commentHeader {
+    background: #030303;
+    box-sizing: border-box;
+    height: 48px;
+    left: 0;
+    margin: 0 auto;
+    max-width: 1068px;
+    position: sticky;
+    right: 0;
+    top: 0;
+    transition: top 0.3s ease;
+    width: calc(100% - 160px);
+    z-index: 1;
+    color: #d7dadc;
+
+    > div {
+      align-items: center;
+      box-sizing: border-box;
+      display: flex;
+      height: 100%;
+      margin: auto;
+      padding: 0 38px 0 57px;
+      width: 100%;
+    }
+  }
+  .likeDiv {
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  .upperTitle {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      line-height: 30px;
+      fill: #d7dadc;
+      cursor: pointer;
+      height: 30px;
+      width: 20px;
+      margin-right: 10px;
+    }
+
+    > div {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      font-size: 14px;
+      font-weight: 500;
+      line-height: 18px;
+    }
+  }
+  .commentClose {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    svg {
+      fill: #d7dadc !important;
+      font-size: 25px;
+      margin-right: 2px;
+    }
+    div {
+      font-size: 13px;
+      font-weight: 600;
+    }
+  }
+`;
+
+const Likes = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 8px 4px 8px 0;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 70px;
+  margin-right: 10px;
+  & > div:nth-child(2) {
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 16px;
+    pointer-events: none;
+    word-break: normal;
+    margin: 0px 5px;
+    color: "#d7dadc";
+  }
+  & > svg {
+    line-height: 30px;
+    fill: #818384;
+    cursor: pointer;
+    opacity: 0.8;
+    height: 30px;
+    width: 20px;
+  }
+  & > svg:nth-child(3) {
+    margin-top: 3px;
+  }
+  & > svg:hover {
+    background: rgb(237, 237, 237);
+    fill: #4b5864;
   }
 `;
 export { CommentsPage };
--- src/Components/HomePage/Feed/FeedItem.jsx
+++ src/Components/HomePage/Feed/FeedItem.jsx
@@ -6,9 +6,9 @@ import { FiGift } from "react-icons/fi";
 import { BsThreeDots } from "react-icons/bs";
 import { RiBookmarkLine } from "react-icons/ri";
 import { useHistory } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
-function FeedItem({ community, comments }) {
+function FeedItem({ community, comments, type = 2 }) {
   const { isLight } = useSelector((state) => state.color);
   const history = useHistory();
   const handleRouteCommunity = (e) => {
@@ -17,12 +17,12 @@ function FeedItem({ community, comments }) {
   };
   const handleRouteUser = (e) => {
     e.stopPropagation();
-    history.push("/u/xyz");
+    history.push("/user/xyz");
   };
   const handleRouteComments = () => {
     history.push("/r/xyz/comments/id");
   };
-  return (
+  return type === 1 ? (
     <Con isLight={isLight}>
       <Likes isLight={isLight}>
         <ImArrowUp />
@@ -110,8 +110,291 @@ function FeedItem({ community, comments }) {
         </div>
       </Box>
     </Con>
-  );
+  ) : type === 2 ? (
+    <Classic isLight={isLight}>
+      <div className="likes">
+        <ImArrowUp />
+        <div>2</div>
+        <ImArrowDown />
+      </div>
+      <div className="postImage">
+        <div
+          style={{
+            backgroundImage:
+              "url(https://preview.redd.it/31ihgjkvv5q71.jpg?width=640&crop=smart&auto=webp&s=48f6206ff4d37e6b26758e415d70576eb11ac1af)",
+          }}
+        ></div>
+      </div>
+      <div className="box">
+        <div className="title">
+          What sdfsdf dfsdfsf sdfsdf fgdg fgd fgdgdfgdfg dfgdg dfgfjh tuk yuk dgdht ykyulkyukgdgg rh
+          yjkyukuy ffj gykuk hulk sdfg
+        </div>
+        <div className="upper">
+          <div className="text">
+            <span
+              onClick={(e) => {
+                handleRouteCommunity(e);
+              }}
+            >
+              {!community && `r/sdfsf`}
+            </span>
+            <span
+              onClick={(e) => {
+                handleRouteUser(e);
+              }}
+            >
+              Posted by r/sdfsdf
+            </span>
+            <span>8 hours ago</span>
+          </div>
+          {!community && !comments && (
+            <div className="join">
+              <HiOutlinePlus />
+            </div>
+          )}
+        </div>
+
+        <div className="comments">
+          <div
+            onClick={() => {
+              handleRouteComments();
+            }}
+            className="icon"
+          >
+            <div>
+              <FaRegCommentAlt />
+            </div>
+            <div>10 Comments</div>
+          </div>
+          <div className="icon">
+            <div>
+              <FiGift />
+            </div>
+            <div>Awards</div>
+          </div>
+          <div className="icon">
+            <div>
+              <ImRedo />
+            </div>
+            <div>Share</div>
+          </div>
+          <div className="icon">
+            <div>
+              <RiBookmarkLine />
+            </div>
+            <div>Share</div>
+          </div>
+          <div className="icon">
+            <div>
+              <BsThreeDots />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Classic>
+  ) : null;
 }
+const Classic = styled.div`
+  position: relative;
+  cursor: pointer;
+  width: 640px;
+  min-height: 100px;
+  box-sizing: border-box;
+  background-color: ${(props) => (props.isLight ? "#FFFFFF" : "#1A1A1B")};
+  border-radius: 4px;
+  border: 1px solid ${(props) => (props.isLight ? "#ccc" : "#343536")};
+  display: flex;
+  margin-bottom: 16px;
+  padding: 8px;
+  align-items: flex-start;
+  justify-content: flex-start;
+  &:hover {
+    border: 1px solid ${(props) => (props.isLight ? "#898989" : "#818384")};
+  }
+
+  .likes {
+    display: flex;
+    flex-direction: column;
+    left: 7px;
+    padding: 8px 4px 8px 0;
+    position: absolute;
+    top: 6px;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 40px;
+    & > div:nth-child(2) {
+      font-size: 12px;
+      font-weight: 700;
+      line-height: 16px;
+      pointer-events: none;
+      word-break: normal;
+      color: ${(props) => (props.isLight ? " rgb(26, 26, 27)" : "#d7dadc")};
+    }
+    & > svg {
+      line-height: 30px;
+      fill: #818384;
+      cursor: pointer;
+      opacity: 0.8;
+      height: 30px;
+      width: 26px;
+    }
+    & > svg:hover {
+      background: rgb(237, 237, 237);
+      fill: #4b5864;
+    }
+  }
+  .postImage {
+    width: 100px;
+    height: max-content;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 8px 0px 0px 40px;
+
+    div {
+      transition: filter 0.5s;
+      height: 72px;
+      width: 96px;
+      border: none;
+      border-radius: 4px;
+      background-position: 50% top;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+  }
+  .box {
+    width: 100%;
+    height: 100%;
+    border-radius: 4px;
+    padding-left: 10px;
+    box-sizing: border-box;
+    max-width: 480px;
+    .upper {
+      font-size: 12px;
+      font-weight: 400;
+      line-height: 16px;
+      display: flex;
+      flex-flow: row nowrap;
+      align-items: center;
+      margin: 0 8px 8px;
+      position: relative;
+    }
+    .profileImg {
+      width: 22px;
+      height: 22px;
+      border-radius: 50%;
+      overflow: hidden;
+      margin-right: 5px;
+    }
+    .text {
+      align-items: center;
+      display: flex;
+      flex-wrap: wrap;
+      /* flex: 1 1 auto; */
+      overflow: hidden;
+      position: relative;
+
+      span {
+        margin-right: 5px;
+        cursor: pointer;
+      }
+      span:hover {
+        text-decoration: underline;
+      }
+      span:nth-child(1) {
+        font-size: 12px;
+        font-weight: 700;
+        line-height: 16px;
+        color: ${(props) => (props.isLight ? " #1c1c1c" : "#d7dadc")};
+        display: inline;
+        line-height: 20px;
+
+        vertical-align: baseline;
+      }
+      span:nth-child(n + 2) {
+        color: #787c7e;
+      }
+    }
+    .join {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: ${(props) => (props.isLight ? "#0079d3" : "#d7dadc")};
+      color: ${(props) => (props.isLight ? "#ffffff" : "#1a1a1b")};
+      fill: ${(props) => (props.isLight ? "#ffffff" : "#1a1a1b")};
+
+      font-size: 14px;
+      font-weight: 700;
+      letter-spacing: unset;
+      line-height: 16px;
+      text-transform: unset;
+
+      box-sizing: border-box;
+      border-radius: 20px;
+      cursor: pointer;
+      svg {
+        width: 18px;
+        height: 18px;
+      }
+    }
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .title {
+      margin: 5px 8px;
+      font-size: 18px;
+      font-weight: 500;
+      line-height: 22px;
+      color: ${(props) => (props.isLight ? "#222222" : "#d7dadc")};
+      text-align: left;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+
+    .comments {
+      align-items: center;
+      display: flex;
+      flex-direction: row;
+      height: 24px;
+      padding-right: 10px;
+      fill: #818384;
+      cursor: pointer;
+      opacity: 0.9 !important;
+      color: #818384;
+      font-weight: 700;
+      font-size: 12px;
+      font-family: Arial, sans-serif;
+      line-height: 16px;
+      .icon {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 8px;
+        word-break: normal;
+      }
+      .icon:hover {
+        background: ${(props) =>
+          props.isLight ? "rgb(237, 237, 237)" : "rgba(215, 218, 220, 0.1)"};
+      }
+      .icon > div:nth-child(1) {
+        font-size: 19px;
+        font-weight: 400;
+        height: 24px;
+        line-height: 20px;
+        vertical-align: middle;
+        width: 20px;
+        margin-right: 7px;
+      }
+      .icon > div {
+        margin-right: 8px;
+        height: 19px;
+      }
+    }
+  }
+`;
 
 const Con = styled.div`
   position: relative;
